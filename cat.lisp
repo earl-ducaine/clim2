8cc9177a33ec5291700d1026d9716bbd0d1a85e4
Earl DuCaine <earl.ducaine-gmail-com>
2016-10-10 20:08:14 -0700

    Cleanup cat.lisp

--- cat.lisp
+++ cat.lisp
@@ -1,7 +1,5 @@
-;;(load "misc/compile-1.lisp")
 
-(eval-when (compile load eval)
-  (load "misc/compile-1.lisp")
-  (setf (sys:gsgc-switch :print) t)
-  (concatenate-it (quote motif-clim))
-  )
+
+(load "misc/compile-1.lisp")
+(setf (sys:gsgc-switch :print) t)
+(concatenate-it (quote motif-clim))
