db4ba934f833d4c86496d4d6104c66f6b2343ed3
Earl DuCaine <earl.ducaine-gmail-com>
2016-10-11 01:07:00 -0700

    Remove old build script dependancies

--- cat.lisp
+++ cat.lisp
@@ -1,6 +1,29 @@
 
 
 (load "misc/compile-1.lisp")
+
+
+(pushnew :ansi-90 *features*)
+
+(setq comp:declared-fixnums-remain-fixnums-switch
+      (named-function |(> speed 2)|
+		      (lambda (safety size speed debug compilation-speed)
+			(declare (ignore safety size debug compilation-speed))
+			(> speed 2))))
+
+(setf (logical-pathname-translations "clim2")
+      (list (list ";**;*.*"
+		  (format nil "~A**/*.*"
+			  (directory-namestring
+			   (make-pathname
+			    :directory
+			    (butlast (pathname-directory
+				      *load-pathname*))))))))
+
+
+(eval-when (compile load eval)
+  (load "sys/sysdcl.lisp"))
+
 (setf (sys:gsgc-switch :print) t)
 
 (defvar sys::*clim-library-search-path* '("/usr/X11/lib/" "/usr/X11R6/lib/" "/usr/local/lib/"
@@ -9,4 +32,4 @@
 (cl:in-package #:user)
 
 (unless (ff:get-entry-point (ff:convert-foreign-name "XmCreateMyDrawingArea"))
-    (load "./climxm.so"))
+  (load "./climxm.so"))
--- misc/compile-1.lisp
+++ misc/compile-1.lisp
@@ -6,26 +6,3 @@
 (in-package :user)
 
 ;; Forgive them, lord, for they know not what they do.
-(eval-when (compile load eval)
-  (pushnew :ansi-90 *features*))
-
-(eval-when (compile load eval)
-  (setq comp:declared-fixnums-remain-fixnums-switch
-	(named-function |(> speed 2)|
-			(lambda (safety size speed debug compilation-speed)
-			  (declare (ignore safety size debug compilation-speed))
-			  (> speed 2)))))
-
-(eval-when (compile load eval)
-;;;; Set up translations so we can find stuff.
-  (setf (logical-pathname-translations "clim2")
-	(list (list ";**;*.*"
-		    (format nil "~A**/*.*"
-			    (directory-namestring
-			     (make-pathname
-			      :directory
-			      (butlast (pathname-directory
-					*load-pathname*)))))))))
-
-(eval-when (compile load eval)
-  (load "sys/sysdcl.lisp"))
