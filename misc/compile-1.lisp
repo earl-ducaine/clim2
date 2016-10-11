223be256c924a419d0fa70ccf3bc0f643f1318d1
Earl DuCaine <earl.ducaine-gmail-com>
2016-10-10 21:30:23 -0700

    Cleanup cat.lisp

--- misc/compile-1.lisp
+++ misc/compile-1.lisp
@@ -5,15 +5,11 @@
 
 (in-package :user)
 
-;;; This should not matter
-;;; (setq *ignore-package-name-case* t)
-
 ;; Forgive them, lord, for they know not what they do.
 (eval-when (compile load eval)
   (pushnew :ansi-90 *features*))
 
 (eval-when (compile load eval)
-  ;; [bug18430]:
   (setq comp:declared-fixnums-remain-fixnums-switch
 	(named-function |(> speed 2)|
 			(lambda (safety size speed debug compilation-speed)
@@ -58,45 +54,6 @@
   (when (probe-file "clim2:;climtoys;sysdcl.lisp")
     (load "clim2:;climtoys;sysdcl")))
 
-;;;; System declarations for compiling, concatenating &c.
-;;;
-;;; This is all horrible.  There are two entangled problems here which
-;;; are both basically artifacts of the fasls that were being built
-;;; with the cat-based makefile.  What is built is a large `generic'
-;;; fasl -- climg -- which does (or should) not have any port-specific
-;;; stuff in it, and then several smaller fasls which have stuff like
-;;; the motif port, the postscript port and so on.  The problems are:
-;;;
-;;; 1. Many of the systems used for building have modules which are
-;;; complete other systems.  So if you try to concate them you get
-;;; huge fasls.  This is OK for climg, but it doesn't work for the
-;;; higher-layer systems.  `cattable' versions of these systems are
-;;; defined below which do not have the dependencies.  It's not enough
-;;; just to have a concatenate-system which does not walk into
-;;; modules, as some of these are two-deep.
-;;;
-;;; 2. The actual fasls that were built did not correspond to systems
-;;; at all.  Typically there was a x.fasl and a debugx.fasl, where
-;;; x.fasl had the stuff you needed at runtime was in the x.fasl and
-;;; compile-time stuff like def-x macros was in the debugx.fasl.  In
-;;; order to get this to work, the systems would need to be split into
-;;; two with foo being foo-compiletime and foo-runtime, and then you'd
-;;; be able to cat them seperately.  Except that some of the debug
-;;; fasls cut across several systems.  What I'm doing about this is to
-;;; essentially not have any debugx fasls.  The debugging code goes
-;;; into the x fasls, and I build empty fasls for the debug stuff.
-;;; This is nasty, but it means that stuff won't break, even if the
-;;; basic image is a little bigger.
-;;;
-;;; PROPOSAL: in a future ACL release we should stop doing all this,
-;;; and just ship one large fasl with everything in for each platform.
-;;; This makes things a little bit bigger, but it seems just a waste
-;;; to spend all this time fiddling.  It's not like CLIM is big any
-;;; more, unless you're running on a 386 or something.
-;;;
-;;; As well as this, the system definitions need looked at more and
-;;; cleaned up.
-
 (eval-when (compile load eval)
   (defsystem climg
     ;; climg is generic clim and ends up as climg.fasl.  This is
@@ -104,7 +61,6 @@
     ()
     (:serial
      clim-standalone			;from sys;sysdcl
-;;     postscript-clim-stubs		;from postscript;sysdcl
      )))
 
 (eval-when (compile load eval)
@@ -114,19 +70,7 @@
     ;; probably make this be just clim-demo + clim-tests.
     ()
     (:serial
-;;     clim-demo				;demo;sysdcl
-;;     "clim2:;test;test-suite"             ;hack!
      )))
-
-;; (eval-when (compile load eval)
-;;   (defsystem hpgl-clim-cat
-;;     ;; a cattable hpgl-clim, see clim2:;hpgl;sysdcl
-;;     (:default-pathname "clim2:;hpgl;")
-;;     (:serial
-;;      ("pkg")
-;;      ("hpgl-port")
-;; ;;     ("hpgl-medium")
-;;      )))
 
 (eval-when (compile load eval)
   (defsystem empty-cat
@@ -139,13 +83,6 @@
     ;; a cattable xlib, see clim2:;sys;sysdcl
     (:default-pathname "clim2:;xlib;")
     (:serial
-     ;; "pkg"
-     ;; "ffi"
-     ;; ("load-xlib")
-     ;; ("xlib-defs" (:load-before-compile "ffi"))
-     ;; ("xlib-funs" (:load-before-compile "ffi"))
-     ;; ("x11-keysyms" (:load-before-compile "ffi"))
-     ;; ("last" (:load-before-compile "load-xlib" "xlib-funs"))
      )))
 
 (defmacro define-xt-cat-system (name file &rest modules)
@@ -158,26 +95,6 @@
      (:serial
       (,file)
       xlib-cat
-      ;; ("pkg")
-      ;; ("macros")
-      ;; ("xt-defs")
-      ;; ("xt-funs")
-      ;; ("foreign-obj")
-      ;; ;; Xlib stuff
-      ;; ("xlib")
-      ;; ("font")
-      ;; ("gcontext")
-      ;; ("graphics")
-      ;; ;; Toolkit stuff
-      ;; ("meta-tk")
-      ;; ("make-classes")
-      ;; ("foreign")
-      ;; ("widget")
-      ;; ("resources")
-      ;; ("event")
-      ;; ("callbacks")
-      ;; ("xt-classes")
-      ;; ("xt-init")
       ,@modules)))
 
 (eval-when (compile load eval)
@@ -201,78 +118,4 @@
     (:default-pathname "clim2:;tk-silica;")
     (:serial
      xm-tk-cat
-     ;; ("pkg")
-     ;; ("xt-silica")
-     ;; ("xt-stipples")
-     ;; ("xm-silica")
-     ;; ("xt-graphics")
-     ;; ("image")
-     ;; ("xt-frames")
-     ;; ("xm-frames")
-     ;; ("xm-dialogs")
-     ;; ("xt-gadgets")
-     ;; ("xm-gadgets")
-     ;; ("xt-pixmaps")
-     ;; ("gc-cursor")
-     ;; last
      )))
-
-;; (eval-when (compile load eval)
-;;   (defsystem wnn-cat
-;;     ;; cattable wnn, see clim2:;sys;sysdcl
-;;     (:default-pathname "clim2:;wnn;")
-;;     (:serial
-;;      "pkg"
-;;      "load-wnn"
-;;      "jl-defs"
-;;      "jl-funs"
-;;      "jserver")))
-
-;;; Compiling a system.
-;;;
-;;; This is just hard-wired -- the makefile says (compile-it
-;;; <something>), which determines which top-level system to build,
-;;; but all the other systems are wired in here.  And currently there
-;;; is only one possible top-level system per platform, unless by some
-;;; miracle the openlook stuff still built!
-(defun compile-it (sys)
-  (flet ((cl (s &key (include-components t)
-		     (ignore-if-unknown nil)
-		     (load-too nil))
-	   (cond ((ignore-errors (excl:find-system s))
-		  (excl:compile-system s
-				       :include-components include-components)
-		  (when load-too
-		    (excl:tenuring
-		     (excl:load-system s))))
-		 ((not ignore-if-unknown)
-		  (error "System ~S not known" s))
-		 (t nil))))
-    (with-compilation-unit ()
-      (cl sys)
-      ;; OK, now we randomly compile some other systems in a very
-      ;; hacky way.  Several of these are just because that's the way
-      ;; it was done before.  As well as platform conditionalisation,
-      ;; the clim-homegrown and the clim-compatibility (from
-      ;; compatibility;sysdcl) systems were not being bult on any
-      ;; platform.
-      ;; I am not sure if this is the right test...
-      ;;;(cl 'wnn)
-      ;; (cl 'postscript-clim)
-      ;; (cl 'climdemo)
-      ;; This currently does not build on windows but I think it
-      ;; should do in future
-      ;;(cl 'testing)
-      (cl 'clim-toys :ignore-if-unknown t)
-      ;;(cl 'hpgl-clim)
-      )))
-
-;;; Concatenating systems
-;;;
-;;; This is fairly hacky as well.  This code *knows* about what
-;;; pathnames to dump systems under.  Again, SYS is just the top-level
-;;; system (it should agree with the one we gave to COMPILE-IT above.
-(defun concatenate-it (sys)
-  (concatenate-system 'motif-clim-cat "clim2:;climxm.fasl")
-  (concatenate-system 'empty-cat "clim2:;clim-debugxm.fasl")
-  (concatenate-system 'empty-cat "clim2:;clim-debug.fasl"))
