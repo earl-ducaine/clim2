77236f84356e4af22a90710a4869fb20b130b10a
Earl DuCaine <earl.ducaine-gmail-com>
2016-10-14 19:12:15 -0700

    Remove *.c warnings and unused code

--- c-src/stub-motif.c
+++ c-src/stub-motif.c
@@ -1,224 +1,228 @@
+#include <Xm/Xm.h>
+
+int lisp_stub_dummy_caller(WidgetClass *var, ...);
+
 void ___lisp_load_motif_stub () {
-  XmStringCreateLtoR();
-  XmStringGetLtoR();
-  XmGetFocusWidget();
-  XmGetPixmap();
-  XmFontListInitFontContext();
-  XmFontListFreeFontContext();
-  XmFontListGetNextFont();
-  XmFontListCreate();
-  XmFontListFree();
-  XmFontListEntryCreate();
-  XmFontListEntryFree();
-  XmFontListAppendEntry();
-  XmFontListEntryGetFont();
-  XmAddProtocolCallback();
-  XmInternAtom();
-  XmIsTraversable();
-  XmMainWindowSetAreas();
-  XmProcessTraversal();
-  XmMessageBoxGetChild();
-  XmFileSelectionDoSearch();
-  XmOptionLabelGadget();
-  XmOptionButtonGadget();
-  XmTextFieldGetSelection();
-  XmSelectionBoxGetChild();
-  XmFontListNextEntry();
-  XmToggleButtonSetState();
-  XmToggleButtonGetState();
-  XmTextGetSelection();
-  XmGetXmDisplay();
+  XmStringCreateLtoR(NULL, NULL);
+  XmStringGetLtoR(NULL, XmFONTLIST_DEFAULT_TAG, NULL);
+  XmGetFocusWidget(NULL);
+  XmGetPixmap(NULL, NULL, 0, 0);
+  XmFontListInitFontContext(NULL, NULL);
+  /* XmFontListFreeFontContext(); */
+  /* XmFontListGetNextFont(); */
+  /* XmFontListCreate(); */
+  /* XmFontListFree(); */
+  /* XmFontListEntryCreate(); */
+  /* XmFontListEntryFree(); */
+  /* XmFontListAppendEntry(); */
+  /* XmFontListEntryGetFont(); */
+  /* XmAddProtocolCallback(); */
+  /* XmInternAtom(); */
+  /* XmIsTraversable(); */
+  /* XmMainWindowSetAreas(); */
+  /* XmProcessTraversal(); */
+  /* XmMessageBoxGetChild(); */
+  /* XmFileSelectionDoSearch(); */
+  /* XmOptionLabelGadget(); */
+  /* XmOptionButtonGadget(); */
+  /* XmTextFieldGetSelection(); */
+  /* XmSelectionBoxGetChild(); */
+  /* XmFontListNextEntry(); */
+  /* XmToggleButtonSetState(); */
+  /* XmToggleButtonGetState(); */
+  /* XmTextGetSelection(); */
+  /* XmGetXmDisplay(); */
 }
 
 void ___lisp_load_motif_stub_vars () {
-  extern int applicationShellWidgetClass;
-  extern int compositeWidgetClass;
-  extern int constraintWidgetClass;
-  extern int coreWidgetClass;
-  extern int objectClass;
-  extern int overrideShellWidgetClass;
-  extern int shellWidgetClass;
-  extern int topLevelShellWidgetClass;
-  extern int transientShellWidgetClass;
-  extern int vendorShellWidgetClass;
-  extern int wmShellWidgetClass;
-  extern int xmArrowButtonGadgetClass;
-  extern int xmArrowButtonWidgetClass;
-  extern int xmBulletinBoardWidgetClass;
-  extern int xmCascadeButtonGadgetClass;
-  extern int xmCascadeButtonWidgetClass;
-  extern int xmCommandWidgetClass;
-  extern int xmDialogShellWidgetClass;
-  extern int xmDrawingAreaWidgetClass;
-  extern int xmDrawnButtonWidgetClass;
-  extern int xmFileSelectionBoxWidgetClass;
-  extern int xmFormWidgetClass;
-  extern int xmFrameWidgetClass;
-  extern int xmGadgetClass;
-  extern int xmLabelGadgetClass;
-  extern int xmLabelWidgetClass;
-  extern int xmListWidgetClass;
-  extern int xmMainWindowWidgetClass;
-  extern int xmManagerWidgetClass;
-  extern int xmMenuShellWidgetClass;
-  extern int xmMessageBoxWidgetClass;
-  extern int xmPanedWindowWidgetClass;
-  extern int xmPrimitiveWidgetClass;
-  extern int xmPushButtonGadgetClass;
-  extern int xmPushButtonWidgetClass;
-  extern int xmRowColumnWidgetClass;
-  extern int xmScaleWidgetClass;
-  extern int xmScrollBarWidgetClass;
-  extern int xmScrolledWindowWidgetClass;
-  extern int xmSelectionBoxWidgetClass;
-  extern int xmSeparatorGadgetClass;
-  extern int xmSeparatorWidgetClass;
-  extern int xmTextFieldWidgetClass;
-  extern int xmTextWidgetClass;
-  extern int xmToggleButtonGadgetClass;
-  extern int xmToggleButtonWidgetClass;
-  extern int applicationShellWidgetClass;
-  extern int compositeWidgetClass;
-  extern int constraintWidgetClass;
-  extern int coreWidgetClass;
-  extern int objectClass;
-  extern int overrideShellWidgetClass;
-  extern int shellWidgetClass;
-  extern int topLevelShellWidgetClass;
-  extern int transientShellWidgetClass;
-  extern int vendorShellWidgetClass;
-  extern int wmShellWidgetClass;
-  extern int xmArrowButtonGadgetClass;
-  extern int xmArrowButtonWidgetClass;
-  extern int xmBulletinBoardWidgetClass;
-  extern int xmCascadeButtonGadgetClass;
-  extern int xmCascadeButtonWidgetClass;
-  extern int xmCommandWidgetClass;
-  extern int xmDialogShellWidgetClass;
-  extern int xmDrawingAreaWidgetClass;
-  extern int xmDrawnButtonWidgetClass;
-  extern int xmFileSelectionBoxWidgetClass;
-  extern int xmFormWidgetClass;
-  extern int xmFrameWidgetClass;
-  extern int xmGadgetClass;
-  extern int xmLabelGadgetClass;
-  extern int xmLabelWidgetClass;
-  extern int xmListWidgetClass;
-  extern int xmMainWindowWidgetClass;
-  extern int xmManagerWidgetClass;
-  extern int xmMenuShellWidgetClass;
-  extern int xmMessageBoxWidgetClass;
-  extern int xmPanedWindowWidgetClass;
-  extern int xmPrimitiveWidgetClass;
-  extern int xmPushButtonGadgetClass;
-  extern int xmPushButtonWidgetClass;
-  extern int xmRowColumnWidgetClass;
-  extern int xmScaleWidgetClass;
-  extern int xmScrollBarWidgetClass;
-  extern int xmScrolledWindowWidgetClass;
-  extern int xmSelectionBoxWidgetClass;
-  extern int xmSeparatorGadgetClass;
-  extern int xmSeparatorWidgetClass;
-  extern int xmTextFieldWidgetClass;
-  extern int xmTextWidgetClass;
-  extern int xmToggleButtonGadgetClass;
-  extern int xmToggleButtonWidgetClass;
-  lisp_stub_dummy_caller(
-			 applicationShellWidgetClass,
-			 compositeWidgetClass,
-			 constraintWidgetClass,
-			 coreWidgetClass,
-			 objectClass,
-			 overrideShellWidgetClass,
-			 shellWidgetClass,
-			 topLevelShellWidgetClass,
-			 transientShellWidgetClass,
-			 vendorShellWidgetClass,
-			 wmShellWidgetClass,
-			 xmArrowButtonGadgetClass,
-			 xmArrowButtonWidgetClass,
-			 xmBulletinBoardWidgetClass,
-			 xmCascadeButtonGadgetClass,
-			 xmCascadeButtonWidgetClass,
-			 xmCommandWidgetClass,
-			 xmDialogShellWidgetClass,
-			 xmDrawingAreaWidgetClass,
-			 xmDrawnButtonWidgetClass,
-			 xmFileSelectionBoxWidgetClass,
-			 xmFormWidgetClass,
-			 xmFrameWidgetClass,
-			 xmGadgetClass,
-			 xmLabelGadgetClass,
-			 xmLabelWidgetClass,
-			 xmListWidgetClass,
-			 xmMainWindowWidgetClass,
-			 xmManagerWidgetClass,
-			 xmMenuShellWidgetClass,
-			 xmMessageBoxWidgetClass,
-			 xmPanedWindowWidgetClass,
-			 xmPrimitiveWidgetClass,
-			 xmPushButtonGadgetClass,
-			 xmPushButtonWidgetClass,
-			 xmRowColumnWidgetClass,
-			 xmScaleWidgetClass,
-			 xmScrollBarWidgetClass,
-			 xmScrolledWindowWidgetClass,
-			 xmSelectionBoxWidgetClass,
-			 xmSeparatorGadgetClass,
-			 xmSeparatorWidgetClass,
-			 xmTextFieldWidgetClass,
-			 xmTextWidgetClass,
-			 xmToggleButtonGadgetClass,
-			 xmToggleButtonWidgetClass,
-			 applicationShellWidgetClass,
-			 compositeWidgetClass,
-			 constraintWidgetClass,
-			 coreWidgetClass,
-			 objectClass,
-			 overrideShellWidgetClass,
-			 shellWidgetClass,
-			 topLevelShellWidgetClass,
-			 transientShellWidgetClass,
-			 vendorShellWidgetClass,
-			 wmShellWidgetClass,
-			 xmArrowButtonGadgetClass,
-			 xmArrowButtonWidgetClass,
-			 xmBulletinBoardWidgetClass,
-			 xmCascadeButtonGadgetClass,
-			 xmCascadeButtonWidgetClass,
-			 xmCommandWidgetClass,
-			 xmDialogShellWidgetClass,
-			 xmDrawingAreaWidgetClass,
-			 xmDrawnButtonWidgetClass,
-			 xmFileSelectionBoxWidgetClass,
-			 xmFormWidgetClass,
-			 xmFrameWidgetClass,
-			 xmGadgetClass,
-			 xmLabelGadgetClass,
-			 xmLabelWidgetClass,
-			 xmListWidgetClass,
-			 xmMainWindowWidgetClass,
-			 xmManagerWidgetClass,
-			 xmMenuShellWidgetClass,
-			 xmMessageBoxWidgetClass,
-			 xmPanedWindowWidgetClass,
-			 xmPrimitiveWidgetClass,
-			 xmPushButtonGadgetClass,
-			 xmPushButtonWidgetClass,
-			 xmRowColumnWidgetClass,
-			 xmScaleWidgetClass,
-			 xmScrollBarWidgetClass,
-			 xmScrolledWindowWidgetClass,
-			 xmSelectionBoxWidgetClass,
-			 xmSeparatorGadgetClass,
-			 xmSeparatorWidgetClass,
-			 xmTextFieldWidgetClass,
-			 xmTextWidgetClass,
-			 xmToggleButtonGadgetClass,
-			 xmToggleButtonWidgetClass,
-			 0);
+  extern WidgetClass applicationShellWidgetClass;
+  extern WidgetClass compositeWidgetClass;
+  extern WidgetClass constraintWidgetClass;
+  extern WidgetClass coreWidgetClass;
+  extern WidgetClass objectClass;
+  extern WidgetClass overrideShellWidgetClass;
+  extern WidgetClass shellWidgetClass;
+  extern WidgetClass topLevelShellWidgetClass;
+  extern WidgetClass transientShellWidgetClass;
+  extern WidgetClass vendorShellWidgetClass;
+  extern WidgetClass wmShellWidgetClass;
+  extern WidgetClass xmArrowButtonGadgetClass;
+  extern WidgetClass xmArrowButtonWidgetClass;
+  extern WidgetClass xmBulletinBoardWidgetClass;
+  extern WidgetClass xmCascadeButtonGadgetClass;
+  extern WidgetClass xmCascadeButtonWidgetClass;
+  extern WidgetClass xmCommandWidgetClass;
+  extern WidgetClass xmDialogShellWidgetClass;
+  extern WidgetClass xmDrawingAreaWidgetClass;
+  extern WidgetClass xmDrawnButtonWidgetClass;
+  extern WidgetClass xmFileSelectionBoxWidgetClass;
+  extern WidgetClass xmFormWidgetClass;
+  extern WidgetClass xmFrameWidgetClass;
+  extern WidgetClass xmGadgetClass;
+  extern WidgetClass xmLabelGadgetClass;
+  extern WidgetClass xmLabelWidgetClass;
+  extern WidgetClass xmListWidgetClass;
+  extern WidgetClass xmMainWindowWidgetClass;
+  extern WidgetClass xmManagerWidgetClass;
+  extern WidgetClass xmMenuShellWidgetClass;
+  extern WidgetClass xmMessageBoxWidgetClass;
+  extern WidgetClass xmPanedWindowWidgetClass;
+  extern WidgetClass xmPrimitiveWidgetClass;
+  extern WidgetClass xmPushButtonGadgetClass;
+  extern WidgetClass xmPushButtonWidgetClass;
+  extern WidgetClass xmRowColumnWidgetClass;
+  extern WidgetClass xmScaleWidgetClass;
+  extern WidgetClass xmScrollBarWidgetClass;
+  extern WidgetClass xmScrolledWindowWidgetClass;
+  extern WidgetClass xmSelectionBoxWidgetClass;
+  extern WidgetClass xmSeparatorGadgetClass;
+  extern WidgetClass xmSeparatorWidgetClass;
+  extern WidgetClass xmTextFieldWidgetClass;
+  extern WidgetClass xmTextWidgetClass;
+  extern WidgetClass xmToggleButtonGadgetClass;
+  extern WidgetClass xmToggleButtonWidgetClass;
+  extern WidgetClass applicationShellWidgetClass;
+  extern WidgetClass compositeWidgetClass;
+  extern WidgetClass constraintWidgetClass;
+  extern WidgetClass coreWidgetClass;
+  extern WidgetClass objectClass;
+  extern WidgetClass overrideShellWidgetClass;
+  extern WidgetClass shellWidgetClass;
+  extern WidgetClass topLevelShellWidgetClass;
+  extern WidgetClass transientShellWidgetClass;
+  extern WidgetClass vendorShellWidgetClass;
+  extern WidgetClass wmShellWidgetClass;
+  extern WidgetClass xmArrowButtonGadgetClass;
+  extern WidgetClass xmArrowButtonWidgetClass;
+  extern WidgetClass xmBulletinBoardWidgetClass;
+  extern WidgetClass xmCascadeButtonGadgetClass;
+  extern WidgetClass xmCascadeButtonWidgetClass;
+  extern WidgetClass xmCommandWidgetClass;
+  extern WidgetClass xmDialogShellWidgetClass;
+  extern WidgetClass xmDrawingAreaWidgetClass;
+  extern WidgetClass xmDrawnButtonWidgetClass;
+  extern WidgetClass xmFileSelectionBoxWidgetClass;
+  extern WidgetClass xmFormWidgetClass;
+  extern WidgetClass xmFrameWidgetClass;
+  extern WidgetClass xmGadgetClass;
+  extern WidgetClass xmLabelGadgetClass;
+  extern WidgetClass xmLabelWidgetClass;
+  extern WidgetClass xmListWidgetClass;
+  extern WidgetClass xmMainWindowWidgetClass;
+  extern WidgetClass xmManagerWidgetClass;
+  extern WidgetClass xmMenuShellWidgetClass;
+  extern WidgetClass xmMessageBoxWidgetClass;
+  extern WidgetClass xmPanedWindowWidgetClass;
+  extern WidgetClass xmPrimitiveWidgetClass;
+  extern WidgetClass xmPushButtonGadgetClass;
+  extern WidgetClass xmPushButtonWidgetClass;
+  extern WidgetClass xmRowColumnWidgetClass;
+  extern WidgetClass xmScaleWidgetClass;
+  extern WidgetClass xmScrollBarWidgetClass;
+  extern WidgetClass xmScrolledWindowWidgetClass;
+  extern WidgetClass xmSelectionBoxWidgetClass;
+  extern WidgetClass xmSeparatorGadgetClass;
+  extern WidgetClass xmSeparatorWidgetClass;
+  extern WidgetClass xmTextFieldWidgetClass;
+  extern WidgetClass xmTextWidgetClass;
+  extern WidgetClass xmToggleButtonGadgetClass;
+  extern WidgetClass xmToggleButtonWidgetClass;
+  /* lisp_stub_dummy_caller( */
+  /* 			 applicationShellWidgetClass, */
+  /* 			 compositeWidgetClass, */
+  /* 			 constraintWidgetClass, */
+  /* 			 coreWidgetClass, */
+  /* 			 objectClass, */
+  /* 			 overrideShellWidgetClass, */
+  /* 			 shellWidgetClass, */
+  /* 			 topLevelShellWidgetClass, */
+  /* 			 transientShellWidgetClass, */
+  /* 			 vendorShellWidgetClass, */
+  /* 			 wmShellWidgetClass, */
+  /* 			 xmArrowButtonGadgetClass, */
+  /* 			 xmArrowButtonWidgetClass, */
+  /* 			 xmBulletinBoardWidgetClass, */
+  /* 			 xmCascadeButtonGadgetClass, */
+  /* 			 xmCascadeButtonWidgetClass, */
+  /* 			 xmCommandWidgetClass, */
+  /* 			 xmDialogShellWidgetClass, */
+  /* 			 xmDrawingAreaWidgetClass, */
+  /* 			 xmDrawnButtonWidgetClass, */
+  /* 			 xmFileSelectionBoxWidgetClass, */
+  /* 			 xmFormWidgetClass, */
+  /* 			 xmFrameWidgetClass, */
+  /* 			 xmGadgetClass, */
+  /* 			 xmLabelGadgetClass, */
+  /* 			 xmLabelWidgetClass, */
+  /* 			 xmListWidgetClass, */
+  /* 			 xmMainWindowWidgetClass, */
+  /* 			 xmManagerWidgetClass, */
+  /* 			 xmMenuShellWidgetClass, */
+  /* 			 xmMessageBoxWidgetClass, */
+  /* 			 xmPanedWindowWidgetClass, */
+  /* 			 xmPrimitiveWidgetClass, */
+  /* 			 xmPushButtonGadgetClass, */
+  /* 			 xmPushButtonWidgetClass, */
+  /* 			 xmRowColumnWidgetClass, */
+  /* 			 xmScaleWidgetClass, */
+  /* 			 xmScrollBarWidgetClass, */
+  /* 			 xmScrolledWindowWidgetClass, */
+  /* 			 xmSelectionBoxWidgetClass, */
+  /* 			 xmSeparatorGadgetClass, */
+  /* 			 xmSeparatorWidgetClass, */
+  /* 			 xmTextFieldWidgetClass, */
+  /* 			 xmTextWidgetClass, */
+  /* 			 xmToggleButtonGadgetClass, */
+  /* 			 xmToggleButtonWidgetClass, */
+  /* 			 applicationShellWidgetClass, */
+  /* 			 compositeWidgetClass, */
+  /* 			 constraintWidgetClass, */
+  /* 			 coreWidgetClass, */
+  /* 			 objectClass, */
+  /* 			 overrideShellWidgetClass, */
+  /* 			 shellWidgetClass, */
+  /* 			 topLevelShellWidgetClass, */
+  /* 			 transientShellWidgetClass, */
+  /* 			 vendorShellWidgetClass, */
+  /* 			 wmShellWidgetClass, */
+  /* 			 xmArrowButtonGadgetClass, */
+  /* 			 xmArrowButtonWidgetClass, */
+  /* 			 xmBulletinBoardWidgetClass, */
+  /* 			 xmCascadeButtonGadgetClass, */
+  /* 			 xmCascadeButtonWidgetClass, */
+  /* 			 xmCommandWidgetClass, */
+  /* 			 xmDialogShellWidgetClass, */
+  /* 			 xmDrawingAreaWidgetClass, */
+  /* 			 xmDrawnButtonWidgetClass, */
+  /* 			 xmFileSelectionBoxWidgetClass, */
+  /* 			 xmFormWidgetClass, */
+  /* 			 xmFrameWidgetClass, */
+  /* 			 xmGadgetClass, */
+  /* 			 xmLabelGadgetClass, */
+  /* 			 xmLabelWidgetClass, */
+  /* 			 xmListWidgetClass, */
+  /* 			 xmMainWindowWidgetClass, */
+  /* 			 xmManagerWidgetClass, */
+  /* 			 xmMenuShellWidgetClass, */
+  /* 			 xmMessageBoxWidgetClass, */
+  /* 			 xmPanedWindowWidgetClass, */
+  /* 			 xmPrimitiveWidgetClass, */
+  /* 			 xmPushButtonGadgetClass, */
+  /* 			 xmPushButtonWidgetClass, */
+  /* 			 xmRowColumnWidgetClass, */
+  /* 			 xmScaleWidgetClass, */
+  /* 			 xmScrollBarWidgetClass, */
+  /* 			 xmScrolledWindowWidgetClass, */
+  /* 			 xmSelectionBoxWidgetClass, */
+  /* 			 xmSeparatorGadgetClass, */
+  /* 			 xmSeparatorWidgetClass, */
+  /* 			 xmTextFieldWidgetClass, */
+  /* 			 xmTextWidgetClass, */
+  /* 			 xmToggleButtonGadgetClass, */
+  /* 			 xmToggleButtonWidgetClass, */
+  /* 			 0); */
 }
 
-int lisp_stub_dummy_caller(i) {
+int lisp_stub_dummy_caller(WidgetClass *var, ...) {
   return 0;
 }
