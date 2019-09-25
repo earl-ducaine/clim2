f35b4e3ba7d914d9af098c1cdd05cd24a088bb7e
Earl Kent <ekent@atlassian.com>
2019-09-24 19:54:02 -0700

    Add hacked path to .so motif wrapper

--- cat-alt.lisp
+++ cat-alt.lisp
@@ -3,7 +3,7 @@
 (eval-when (eval load compile)
   (cffi:define-foreign-library lib-common
     (:unix
-     "/mnt/seagate/working/allegro-clim2-ubuntu-16-04-x32/dev/clim2/liblib_motif_wrapper.so"))
+     "/home/rett/dev/common-lisp/clim2/liblib_motif_wrapper.so"))
 
   (cffi:use-foreign-library lib-common))
 
