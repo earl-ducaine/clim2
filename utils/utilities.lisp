f7225583c378b946218594ae45ee42023644eb05
Earl DuCaine <earl.ducaine-gmail-com>
2019-09-26 17:19:49 -0700

    Cleanup conditional compilation code

--- utils/utilities.lisp
+++ utils/utilities.lisp
@@ -3,44 +3,45 @@
 ;;
 
 ;;;
-;;; Copyright (c) 1989, 1990 by Xerox Corporation.  All rights reserved. 
+;;; Copyright (c) 1989, 1990 by Xerox Corporation.  All rights reserved.
 ;;;
 
 (in-package :clim-utils)
 
 ;;;
 ;;; Various UTILITIES
-;;; 
+;;;
 
 ;;;
 ;;; COMMONLISP Extensions
-;;; 
+;;;
 
 (defmacro compile-time-warn (warn-string)
   (warn warn-string)
   nil)
 
-(eval-when (compile load eval)
-(defmacro with-collection (&body body)
-  `(let (($with-collection-result$ nil)
-         $with-collection-tail$)
-     (macrolet
-         ((collect (form)
-            ;;  The FORM is evaluated first so that COLLECT nests
-            ;; properly, i.e., The test to determine if this is
-            ;; the first value collected should be done after the
-            ;; value itself is generated in case it does
-            ;; collection as well.
-            `(let (($collectable$ ,form))
-               (if $with-collection-tail$
-                   (rplacd $with-collection-tail$
-                           (setq $with-collection-tail$
-                                 (list $collectable$)))
-                   (setq $with-collection-result$
-                         (setq $with-collection-tail$
-                               (list $collectable$))))
-               $with-collection-tail$)))
-       ,@body $with-collection-result$))))
+;;(eval-when (compile load eval)
+  (defmacro with-collection (&body body)
+    `(let (($with-collection-result$ nil)
+	   $with-collection-tail$)
+       (macrolet
+	   ((collect (form)
+	      ;;  The FORM is evaluated first so that COLLECT nests
+	      ;; properly, i.e., The test to determine if this is
+	      ;; the first value collected should be done after the
+	      ;; value itself is generated in case it does
+	      ;; collection as well.
+	      `(let (($collectable$ ,form))
+		 (if $with-collection-tail$
+		     (rplacd $with-collection-tail$
+			     (setq $with-collection-tail$
+				   (list $collectable$)))
+		     (setq $with-collection-result$
+			   (setq $with-collection-tail$
+				 (list $collectable$))))
+		 $with-collection-tail$)))
+	 ,@body $with-collection-result$)))
+;;;)
 
 (defmacro with-gensyms ((&rest vars) &body body)
   `(let ,(mapcar #'(lambda (symbol)
@@ -51,29 +52,28 @@
 (defmacro with-fast-vector-references ((&rest macros-and-arrays) &body body)
   (flet ((simple-part-accessor (array)
            ;; Allegro only allows SVREF on simple T vectors.
-           #+allegro `(let ((temp ,array))
-                        (etypecase temp
-                          (simple-vector temp)
-                          ((vector t)
-                           (let ((temp2 (excl::ah_data temp)))
-                             (setq temp2 (if (consp temp2)
-                                             (cdr temp2)
-                                             temp2))
-                             (assert (and 
-                                       (zerop (the fixnum (excl::ah_displacement temp)))
-                                       (typep temp2 'simple-vector))
-                                     ()
-                                     "Arrays passed to ~S must be non-displaced"
-                                     'with-fast-vector-references)
-                             temp2))))
-           #+Genera array
-           #-(or allegro Genera) array)
+           ;; #+allegro `(let ((temp ,array))
+           ;;              (etypecase temp
+           ;;                (simple-vector temp)
+           ;;                ((vector t)
+           ;;                 (let ((temp2 (excl::ah_data temp)))
+           ;;                   (setq temp2 (if (consp temp2)
+           ;;                                   (cdr temp2)
+           ;;                                   temp2))
+           ;;                   (assert (and
+           ;;                             (zerop (the fixnum (excl::ah_displacement temp)))
+           ;;                             (typep temp2 'simple-vector))
+           ;;                           ()
+           ;;                           "Arrays passed to ~S must be non-displaced"
+           ;;                           'with-fast-vector-references)
+           ;;                   temp2))))
+	   array)
          (internal-binding-declarations (variables)
-           #+allegro `(declare (simple-vector ,@variables))
-           #+Genera `(declare (sys:array-register ,@variables))
-           #-(or allegro Genera) `(declare)))
-    (let* ((aref #+(or allegro Genera) 'svref
-                 #-(or allegro Genera) 'aref)
+           ;; #+allegro `(declare (simple-vector ,@variables))
+           ;; #+Genera `(declare (sys:array-register ,@variables))
+	   `(declare)))
+    (let* ((aref ;; #+(or allegro Genera) 'svref
+	    'aref)
            (macro-names (mapcar #'first macros-and-arrays))
            (internal-variables (mapcar #'gensymbol macro-names))
            (arrays (mapcar #'second macros-and-arrays))
@@ -81,7 +81,7 @@
                                  `(,variable ,(simple-part-accessor array)))
                              internal-variables arrays))
            (macros (mapcar #'(lambda (macro-name variable)
-                               `(,macro-name (index) 
+                               `(,macro-name (index)
                                   `(,',aref ,',variable (the fixnum ,index))))
                            macro-names internal-variables)))
   `(let ,bindings
@@ -101,8 +101,8 @@
      (defmacro ,name (object key)
        `(getf (slot-value ,object ',',slot-name) ,key))
      (defsetf ,name (object key) (val)
-       `(setf 
-         (getf (slot-value ,object ',',slot-name) ,key) 
+       `(setf
+         (getf (slot-value ,object ',',slot-name) ,key)
          ,val))))
 
 (defmacro def-property-slot-accessors (name (class) slot-name)
@@ -113,7 +113,7 @@
        (setf (getf (slot-value object ',slot-name) key) val))))
 
 ;;;
-;;; Stub Sealers 
+;;; Stub Sealers
 ;;;
 
 (defun unimplemented (&optional (string "So who you gun-na call..."))
@@ -129,9 +129,9 @@
                                                (eq (second lambda-var) protocol-name)))
                                       lambda-list))))
     `(defmethod ,name ,lambda-list
-                #+Genera (declare 
-                          (sys:function-parent ,name
-                                               define-unimplemented-protocol-method))
+                ;; #+Genera (declare
+                ;;           (sys:function-parent ,name
+                ;;                                define-unimplemented-protocol-method))
        (progn ,@variables)                ;Ignore these variables...
        (error "The required operation ~S is not defined for the~
                ~@[ ~S implementation of~] protocol ~S"
